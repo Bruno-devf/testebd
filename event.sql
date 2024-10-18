3fbdff989a60605577ce846fa5e9061471bfe860
Bruno-devf <fonsecabruno6062@gmailcom>
2024-10-17 22:42:12 -0300

    upd

--- event.sql
+++ event.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 11/10/2024 às 03:34
+-- Tempo de geração: 18/10/2024 às 03:40
 -- Versão do servidor: 10.4.28-MariaDB
 -- Versão do PHP: 8.2.4
 
@@ -18,110 +18,49 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Banco de dados: `edu_events`
+-- Banco de dados: `events`
 --
 
 -- --------------------------------------------------------
 
 --
--- Estrutura para tabela `tb_evento`
+-- Estrutura para tabela `testes`
 --
 
-CREATE TABLE `tb_evento` (
-  `id_evento` int(100) NOT NULL,
-  `nome_evento` varchar(255) DEFAULT NULL,
-  `desc` int(255) DEFAULT NULL,
-  `data` varchar(255) DEFAULT NULL,
-  `local` int(255) DEFAULT NULL
+CREATE TABLE `testes` (
+  `id` int(11) NOT NULL,
+  `nome` varchar(255) DEFAULT NULL,
+  `sobrenome` text DEFAULT NULL,
+  `createdAt` datetime NOT NULL,
+  `updatedAt` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Despejando dados para a tabela `tb_evento`
+-- Despejando dados para a tabela `testes`
 --
 
-INSERT INTO `tb_evento` (`id_evento`, `nome_evento`, `desc`, `data`, `local`) VALUES
-(1, 'festa junina', NULL, '10/10/10', 96837500),
-(2, 'arrecadação de fundos (HALLOWWEEN)', NULL, '11/11/11', 69316124),
-(12, 'Doação', NULL, '12/12/12', 96837500);
-
--- --------------------------------------------------------
-
---
--- Estrutura para tabela `tb_professor`
---
-
-CREATE TABLE `tb_professor` (
-  `id_professor` int(10) NOT NULL,
-  `nome_completo` varchar(50) DEFAULT NULL,
-  `RG` int(30) DEFAULT NULL,
-  `email` varchar(50) DEFAULT NULL,
-  `senha` varchar(255) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Despejando dados para a tabela `tb_professor`
---
-
-INSERT INTO `tb_professor` (`id_professor`, `nome_completo`, `RG`, `email`, `senha`) VALUES
-(3, 'Frederico Casmurro', 2415765, 'fred@casm.com', NULL),
-(9, 'Geraldo Rolisso', 65168516, 'geraldo@roll.com', NULL);
-
--- --------------------------------------------------------
-
---
--- Estrutura para tabela `tb_relatorio`
---
-
-CREATE TABLE `tb_relatorio` (
-  `id_relatorio` int(10) DEFAULT NULL,
-  `nome_aluno` varchar(50) DEFAULT NULL,
-  `id_evento` int(11) DEFAULT NULL,
-  `id_professor` int(11) DEFAULT NULL,
-  `descr` varchar(100) DEFAULT NULL,
-  `componentes` varchar(255) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Despejando dados para a tabela `tb_relatorio`
---
-
-INSERT INTO `tb_relatorio` (`id_relatorio`, `nome_aluno`, `id_evento`, `id_professor`, `descr`, `componentes`) VALUES
-(25, 'junina', 1, 3, 'nesse evento terá muita comida e caracterização', 'Alunos, Professores e familiares'),
-(24, 'halloween', 2, 9, 'evento beneficente com venda de doces e rifas  ', 'Toda a comunidade em volta');
+INSERT INTO `testes` (`id`, `nome`, `sobrenome`, `createdAt`, `updatedAt`) VALUES
+(1, 'kauê', 'Mendes', '2024-10-18 01:38:18', '2024-10-18 01:38:18');
 
 --
 -- Índices para tabelas despejadas
 --
 
 --
--- Índices de tabela `tb_evento`
+-- Índices de tabela `testes`
 --
-ALTER TABLE `tb_evento`
-  ADD PRIMARY KEY (`id_evento`);
+ALTER TABLE `testes`
+  ADD PRIMARY KEY (`id`);
 
 --
--- Índices de tabela `tb_professor`
---
-ALTER TABLE `tb_professor`
-  ADD PRIMARY KEY (`id_professor`);
-
---
--- Índices de tabela `tb_relatorio`
---
-ALTER TABLE `tb_relatorio`
-  ADD KEY `id_evento` (`id_evento`),
-  ADD KEY `id_professor` (`id_professor`);
-
---
--- Restrições para tabelas despejadas
+-- AUTO_INCREMENT para tabelas despejadas
 --
 
 --
--- Restrições para tabelas `tb_relatorio`
+-- AUTO_INCREMENT de tabela `testes`
 --
-ALTER TABLE `tb_relatorio`
-  ADD CONSTRAINT `tb_relatorio_ibfk_1` FOREIGN KEY (`id_evento`) REFERENCES `tb_evento` (`id_evento`),
-  ADD CONSTRAINT `tb_relatorio_ibfk_2` FOREIGN KEY (`id_professor`) REFERENCES `tb_professor` (`id_professor`);
+ALTER TABLE `testes`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
